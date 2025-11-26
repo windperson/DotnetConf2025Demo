149ab0bb7bf70abc4cdfc8c35bc022a7a96bb82d
Isak Pao <windperson@gmail.com>
2025-11-27 01:19:46 +0800

    Rule based Agent demo

---
+++ SimpleReverseInputAgentDemo/ConsoleDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using AutoGen;
+using AutoGen.Core;
+using SimpleReverseInputAgent;
+
+var userProxyAgent = new UserProxyAgent(
+        name: "user-proxy-agent",
+        humanInputMode: HumanInputMode.ALWAYS)
+    .RegisterPrintMessage();
+
+var demoReverseAgent = new ReverseInputAgent(name: "reverse-agent")
+    .RegisterPrintMessage();
+
+await userProxyAgent.InitiateChatAsync(receiver: demoReverseAgent);
---
+++ SimpleReverseInputAgentDemo/SimpleReverseInputAgent/ReverseInputAgent.cs
@@ -0,0 +1,23 @@
+﻿using AutoGen.Core;
+
+namespace SimpleReverseInputAgent;
+
+public class ReverseInputAgent(string name) : IAgent
+{
+    public string Name { get; } = name;
+
+    public Task<IMessage> GenerateReplyAsync(IEnumerable<IMessage> messages, GenerateReplyOptions? options = null,
+        CancellationToken cancellationToken = new())
+    {
+        if (messages.LastOrDefault() is not TextMessage lastMessage)
+        {
+            throw new InvalidOperationException("No messages to reply to.");
+        }
+
+        // Reverse the content of the last message
+        var reversedContent = new string(lastMessage.Content?.ToCharArray().Reverse().ToArray());
+        IMessage replyMessage = new TextMessage(Role.Assistant, reversedContent, from: Name);
+
+        return Task.FromResult(replyMessage);
+    }
+}
