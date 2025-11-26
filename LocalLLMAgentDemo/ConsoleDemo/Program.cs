c33d6fc0c2da056f3a2d3ed6ff3ee68fe3685ee6
Isak Pao <windperson@gmail.com>
2025-11-27 07:59:05 +0800

    Call local LMStudio openai/gpt-oss-20b example code.

---
+++ LocalLLMAgentDemo/ConsoleDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using AutoGen.Core;
+using LLMStudioAgent;
+using OpenAI.Chat;
+
+var localAgent = CreateAgentHelper.CreateLMStudioAgent("demo-local-llm-agent", modelName: "openai/gpt-oss-20b");
+
+// Need to convert the message text into a UserChatMessage (OpenAI Type object)
+// Then wrap in a MessageEnvelope, otherwise, OpenAIChatClient will reject it as "System.ArgumentException: Invalid message type."
+IMessage<ChatMessage> requestMessage =
+    new MessageEnvelope<ChatMessage>(ChatMessage.CreateUserMessage("Hello, what can you do?"));
+
+var received = await localAgent.SendAsync(requestMessage);
+
+if (received is MessageEnvelope<ChatCompletion> envelope)
+{
+    var messageContentPart = envelope.Content.Content.FirstOrDefault();
+    if (messageContentPart != null)
+    {
+        Console.WriteLine("Received response:\r\n");
+        Console.WriteLine(messageContentPart.Text);
+    }
+}
---
+++ LocalLLMAgentDemo/LLMStudioAgent/CreateAgentHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.ClientModel;
+using AutoGen.Core;
+using AutoGen.OpenAI;
+using AutoGen.OpenAI.Extension;
+using OpenAI;
+
+namespace LLMStudioAgent;
+
+public static class CreateAgentHelper
+{
+    // ReSharper disable once InconsistentNaming
+    public static IAgent CreateLMStudioAgent(string agentName, string modelName,
+        string systemPrompt = "",
+        string lmStudioEndpoint = "http://localhost:1234/v1")
+    {
+        var openAiClient = new OpenAIClient(new ApiKeyCredential("api-key"), new OpenAIClientOptions
+        {
+            Endpoint = new Uri(lmStudioEndpoint)
+        });
+
+        var chatClient = openAiClient.GetChatClient(modelName);
+
+        var localAgent = string.IsNullOrEmpty(systemPrompt)
+            ? new OpenAIChatAgent(chatClient: chatClient, agentName)
+            : new OpenAIChatAgent(chatClient: chatClient, agentName, systemPrompt);
+
+        localAgent.RegisterMessageConnector()
+            .RegisterPrintMessage();
+
+        return localAgent;
+    }
+}
