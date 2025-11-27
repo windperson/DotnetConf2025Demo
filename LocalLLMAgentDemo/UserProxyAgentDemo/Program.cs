ce948af8f8e1f55de67954f6cea8dfbe76f60cd0
Isak Pao <windperson@gmail.com>
2025-11-28 03:11:47 +0800

    Add token usage display

--- LocalLLMAgentDemo/UserProxyAgentDemo/Program.cs
+++ LocalLLMAgentDemo/UserProxyAgentDemo/Program.cs
@@ -2,8 +2,8 @@
 using AutoGen;
 using AutoGen.Core;
 using AutoGen.OpenAI;
-using AutoGen.OpenAI.Extension;
 using OpenAI;
+using OpenAI.Chat;
 
 var openAiClient = new OpenAIClient(new ApiKeyCredential("api-key"), new OpenAIClientOptions
 {
@@ -14,7 +14,29 @@ var chatClient = openAiClient.GetChatClient("openai/gpt-oss-20b");
 
 var openAiAgent = new OpenAIChatAgent(chatClient: chatClient, name: "local-llm-agent",
         systemMessage: "你是個總是完全都用繁體中文回覆的 AI Assistant。")
-    .RegisterMessageConnector()
+    .RegisterMiddleware(async (messages, option, agent, cancellationToken) =>
+        {
+            var reply = await agent.GenerateReplyAsync(messages, option, cancellationToken);
+
+            if (reply is not MessageEnvelope<ChatCompletion> chatCompletion)
+            {
+                return reply;
+            }
+
+            var inputTokenCount = chatCompletion.Content.Usage.InputTokenCount;
+            var outputTokenCount = chatCompletion.Content.Usage.OutputTokenCount;
+            var totalTokenCount = chatCompletion.Content.Usage.TotalTokenCount;
+
+            Console.WriteLine("====== Token Usage ======");
+            Console.WriteLine($"Input  token count: {inputTokenCount}");
+            Console.WriteLine($"Output token count: {outputTokenCount}");
+            Console.WriteLine($"Total  token count: {totalTokenCount}");
+            Console.WriteLine("=========================\r\n");
+
+            return reply;
+        }
+    )
+    .RegisterMiddleware(new OpenAIChatRequestMessageConnector())
     .RegisterPrintMessage();
 
 var userProxyAgent = new UserProxyAgent(
